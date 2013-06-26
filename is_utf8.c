25c06d7691693ea99395ce98205b63cef3b24a51
Bohdan Kmit <bkmit@maytech.net>
2013-06-26 15:42:08 +0300

    report index of start byte of invalid multibyte sequence instead of first non-utf-8 conforming

--- is_utf8.c
+++ is_utf8.c
@@ -19,29 +19,32 @@ int is_utf8(unsigned char *str, size_t len)
 {
     size_t i = 0;
     size_t continuation_bytes = 0;
+    size_t last = 0;
 
     while (i < len)
     {
         if (str[i] <= 0x7F)
             continuation_bytes = 0;
-        else if (str[i] >= 0xC0 /*11000000*/ && str[i] <= 0xDF /*11011111*/)
+        else if (str[i] >= 0xC0 /*11000000*/ && str[i] <= 0xDF /*11011111*/) {
             continuation_bytes = 1;
-        else if (str[i] >= 0xE0 /*11100000*/ && str[i] <= 0xEF /*11101111*/)
+            last = i;
+        } else if (str[i] >= 0xE0 /*11100000*/ && str[i] <= 0xEF /*11101111*/) {
             continuation_bytes = 2;
-        else if (str[i] >= 0xF0 /*11110000*/ && str[i] <= 0xF4 /* Cause of RFC 3629 */)
+            last = i;
+        } else if (str[i] >= 0xF0 /*11110000*/ && str[i] <= 0xF4 /* Cause of RFC 3629 */) {
             continuation_bytes = 3;
-        else
+            last = i;
+        } else
             return i + 1;
         i += 1;
         while (i < len && continuation_bytes > 0
                && str[i] >= 0x80
-               && str[i] <= 0xBF)
-        {
+               && str[i] <= 0xBF) {
             i += 1;
             continuation_bytes -= 1;
         }
         if (continuation_bytes != 0)
-            return i + 1;
+            return last + 1;
     }
     return 0;
 }
